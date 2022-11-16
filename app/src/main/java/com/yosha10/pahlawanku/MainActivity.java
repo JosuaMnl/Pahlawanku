ee23f1f28bbb5484b547e3fe871f7b63c5d4def9
JosuaMnl <josuamnalu@gmail.com>
2022-11-16 17:16:08 +0700

    Meghubungkan Adapter dengan Main

--- app/src/main/java/com/yosha10/pahlawanku/MainActivity.java
+++ app/src/main/java/com/yosha10/pahlawanku/MainActivity.java
@@ -1,14 +1,33 @@
 package com.yosha10.pahlawanku;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvPahlawan;
+    private ArrayList<ModelPahlawan> data = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvPahlawan = findViewById(R.id.rv_pahlawan);
+        rvPahlawan.setHasFixedSize(true); // Karena telah mengetahui jumlah data pahlawan
+
+        data.addAll(DataPahlawan.ambilDataPahlawan()); // Mengisi variabel data dengan semua data pahlawan dari class Data Pahlawan
+        tampilDataCard();
+    }
+
+    // Menyambungkan dengan Adapter
+    private void tampilDataCard(){
+        rvPahlawan.setLayoutManager(new LinearLayoutManager(this));
+        AdapterCard AC = new AdapterCard(data); // MenColokan Adapter
+        rvPahlawan.setAdapter(AC);
     }
 }
