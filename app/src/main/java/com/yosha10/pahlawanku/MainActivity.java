dd770453eda63a13fa5dfe10e0e037ecebfa0b20
JosuaMnl <josuamnalu@gmail.com>
2022-11-28 14:59:18 +0700

    Membuat tampil data Grid dan setting pilihan menu jika dipilih

--- app/src/main/java/com/yosha10/pahlawanku/MainActivity.java
+++ app/src/main/java/com/yosha10/pahlawanku/MainActivity.java
@@ -1,10 +1,14 @@
 package com.yosha10.pahlawanku;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -22,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
         data.addAll(DataPahlawan.ambilDataPahlawan()); // Mengisi variabel data dengan semua data pahlawan dari class Data Pahlawan
         tampilDataCard();
+        tampilDataGrid();
     }
 
     // Menyambungkan dengan Adapter
@@ -29,5 +34,30 @@ public class MainActivity extends AppCompatActivity {
         rvPahlawan.setLayoutManager(new LinearLayoutManager(this));
         AdapterCard AC = new AdapterCard(data, MainActivity.this); // MenColokan Adapter
         rvPahlawan.setAdapter(AC);
+    }
+
+    private void tampilDataGrid(){
+        rvPahlawan.setLayoutManager(new GridLayoutManager(this, 2));
+        AdapterGrid AG = new AdapterGrid(data, MainActivity.this);
+        rvPahlawan.setAdapter(AG);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_tampilan, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_card:
+                tampilDataCard();
+                break;
+            case R.id.menu_grid:
+                tampilDataGrid();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
